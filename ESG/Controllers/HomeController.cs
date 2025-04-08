fffae76842e223cc143d34b5bd5a546f3f748a87
Wallacevr <wallacevr@gmail.com>
2025-04-07 23:59:52 -0300

    Inicio

---
+++ ESG/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ESG.Models;
+
+namespace ESG.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Ongs()
+    {
+        return View();
+    }
+
+
+    public IActionResult Cadongs()
+    {
+        return View();
+    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
